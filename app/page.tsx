8c8ccd65448370816944b443026c93a604d2e73d
trityazd <161912675+trityazd@users.noreply.github.com>
2024-03-02 22:06:18 +1100

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -20,7 +20,7 @@ export default function Home() {
           -Help resolve billing issues for customers<br></br>
           -Work with engineering teams during incidents and provide updates to internal and external stakeholders<br></br><br></br>
        
-        5 Least Support Tasks:<br></br>
+   <code className="font-mono font-bold">        5 Least Support Tasks: </code>    <br></br> 
           -Engage multiple users at once in a public discussion, to answer their questions and troubleshoot problems:
           If not handled the right way this could lead to more confusion as there is scope for mis-information being shared<br></br>
           
@@ -59,16 +59,11 @@ export default function Home() {
         </div> 
       </div>
       
-      {/*   <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-           <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div> */}
+        <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
+          
+2. What do you want to learn or do more of at work? <br> </br>
+          Learn new technical skills and software tools that is being used widely such as Salesforce, React ; etc.
+</div> 
 
       
 
